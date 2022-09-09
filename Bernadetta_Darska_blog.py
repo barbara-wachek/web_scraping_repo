0d8ccfe91b1bbcd98ed4951cca6706b7656bdf55
Basia <barbara.wachek@gmail.com>
2022-09-09 09:23:19 +0200

    2022-09-09

--- Bernadetta_Darska_blog.py
+++ Bernadetta_Darska_blog.py
@@ -9,8 +9,9 @@ from tqdm import tqdm  #licznik
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 import json
-import functions as fun
-
+from functions import date_change_format_long, get_links
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 #%% def    
 def get_article_pages(link):   
@@ -28,47 +29,75 @@ def dictionary_of_article(article_link):
     
     author = "Bernadetta Darska"
     title_of_article = soup.find('h3', class_='post-title entry-title').text.strip()
-    title_of_book = re.sub(r'(.*\()([\w\.]*\s.*)(\,\s)(\p{Lu}.*)(\)$)', r'\4', title_of_article)     
     date_of_publication = soup.find('h2', class_='date-header').text
-    new_date = fun.date_change_format_long(date_of_publication)
-    texts_of_article = soup.find('div', class_='post-body entry-content')
-    article = texts_of_article.text.strip().replace('\n', ' ')
+    new_date = date_change_format_long(date_of_publication)
+    text_of_article = soup.find('div', class_='post-body entry-content')
+    article = text_of_article.text.strip().replace('\n', ' ').replace('\n', '')
     tags_span = soup.find_all('span', class_='post-labels')
-    tags = [tag.text for tag in tags_span][0].strip().split('\n')    
+    tags = '| '.join([tag.text.replace('\n', '').replace('Etykiety:', '') for tag in tags_span])
+   
     
-    dictionary_of_article = {}
-
     try:
+        book_description = re.findall(r'(?<=\s{3}|\s{4}|\s{5}|\s{6}).*', article)[0].strip()
+    except (AttributeError, KeyError, IndexError, TypeError):
+        book_description = None  
         
-        dictionary_of_article['Link'] = article_link
-        dictionary_of_article['Autor'] = author
-        dictionary_of_article['Tytuł artykułu'] = title_of_article
-        dictionary_of_article['Data publikacji'] = new_date  
-        dictionary_of_article['Tekst artykułu'] = article.replace('\n', ' ')
+    try:   
+        author_of_book = re.findall(r'^[\p{L}\s\-\.]+(?=\,)', book_description)[0].strip()
+    except (AttributeError, KeyError, IndexError, TypeError):
+        author_of_book = None  
         
-        book_description = re.findall(r'(?<=\s{3}|\s{4}|\s{5}|\s{6}).*', article)[0]    
-        dictionary_of_article['Opis książki'] = book_description.strip()
+    try:    
+        title_of_book = re.sub(r'(.*\()([\w\.]*\s.*)(\,\s)(\p{Lu}.*)(\)$)', r'\4', title_of_article) 
+    except (AttributeError, KeyError, IndexError, TypeError):
+        title_of_book = None   
         
-        dictionary_of_article['Autor książki'] = re.findall(r'^[\p{L}\s\-\.]+(?=\,)', book_description)[0].strip()
-        dictionary_of_article['Tytuł książki'] = title_of_book
-        
+    try:
         publisher = re.sub(r'(.*)(\,\s)([\w\s\-\.]*)(\,\s)(.*)(\.$)', r'\3', book_description)
-        dictionary_of_article['Wydawnictwo'] = publisher
+    except (AttributeError, KeyError, IndexError, TypeError):
+        publisher = None
+    
+    try:
+        place_and_year = re.sub(r'(.*)(\,\s)([\w\s\-\.]*)(\,\s)(.*)(\.$)', r'\5', book_description)  
+    except (AttributeError, KeyError, IndexError, TypeError):
+        place_and_year = None
         
-        place_and_year = re.sub(r'(.*)(\,\s)([\w\s\-\.]*)(\,\s)(.*)(\.$)', r'\5', book_description)    
-        dictionary_of_article['Rok i miejsce wydania'] = place_and_year
+    try:
+        external_links = ' | '.join([x for x in [x['href'] for x in text_of_article.find_all('a')] if not re.findall(r'blogger|blogspot|berandettadarska', x)])
+    except (AttributeError, KeyError, IndexError):
+        external_links = None
         
-        dictionary_of_article['Tagi'] = '| '.join(tags[1:]).replace(',', ' ')
+    try: 
+        photos_links = ' | '.join([x['src'] for x in text_of_article.find_all('img')])  
+    except (AttributeError, KeyError, IndexError):
+        photos_links = None
+
+    dictionary_of_article = {'Link': article_link,
+                             'Data publikacji': new_date,
+                             'Autor': author,
+                             'Tytuł artykułu': title_of_article,
+                             'Tekst artykułu': article,
+                             'Opis książki': book_description, 
+                             'Autor książki': author_of_book, 
+                             'Tytuł książki': title_of_book,
+                             'Wydawnictwo': publisher, 
+                             'Rok i miejsce wydania': place_and_year, 
+                             'Tagi': tags,
+                             'Linki zewnętrzne': external_links,
+                             'Zdjęcia/Grafika': True if [x['src'] for x in text_of_article.find_all('img')] else False,
+                             'Filmy': True if [x['src'] for x in text_of_article.find_all('iframe')] else False,
+                             'Linki do zdjęć': photos_links}
         
-    except AttributeError:
-        pass 
-    except IndexError:   
-        pass
+
     all_results.append(dictionary_of_article)
 
 
 #%% main
-sitemap_links = fun.get_links('https://bernadettadarska.blogspot.com/sitemap.xml')
+sitemap_links = get_links('https://bernadettadarska.blogspot.com/sitemap.xml')
+
+articles_links = []    
+with ThreadPoolExecutor() as excecutor:
+    list(tqdm(excecutor.map(get_article_pages, sitemap_links),total=len(sitemap_links)))
    
 all_results = []
 with ThreadPoolExecutor() as excecutor:
@@ -78,15 +107,27 @@ with open(f'bernadetta_darska_{datetime.today().date()}.json', 'w', encoding='ut
     json.dump(all_results, f)    
 
 df = pd.DataFrame(all_results).drop_duplicates()
-df["Data publikacji"] = pd.to_datetime(df["Data publikacji"])
+df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
 df = df.sort_values('Data publikacji', ascending=False)
    
-with pd.ExcelWriter(f"bernadetta_darska__{datetime.today().date()}.xlsx", engine='xlsxwriter') as writer:    
+with pd.ExcelWriter(f"bernadetta_darska_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
     df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
     writer.save()     
    
-   
-   
+
+#%%Uploading files on Google Drive
+
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)   
+      
+upload_file_list = [f"bernadetta_darska_{datetime.today().date()}.xlsx", f'bernadetta_darska_{datetime.today().date()}.json']
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'parents': [{'id': '19t1szTXTCczteiKfF2ukYsuiWpDqyo8f'}]})  
+	gfile.SetContentFile(upload_file)
+	gfile.Upload()  
+
+
+
    
    
    
