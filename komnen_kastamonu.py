3c6ee9d18862c781c724c4a92a714ba6c5958776
Basia <barbara.wachek@gmail.com>
2022-09-12 10:42:17 +0200

    2022-09-12

--- komnen_kastamonu.py
+++ komnen_kastamonu.py
@@ -8,13 +8,13 @@ import time
 from tqdm import tqdm  #licznik
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-from time import mktime
 import json
-
+from functions import date_change_format_long
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 #%%def
 def komnem_kastamonu_web_scraping_sitemap(sitemap):
-   
     html_text_sitemap = requests.get(sitemap).text
     soup = BeautifulSoup(html_text_sitemap, 'lxml')
     links = [e.text for e in soup.find_all('loc')]
@@ -27,111 +27,84 @@ def get_article_pages(link):
     articles_links.extend(sitemap_links)  
     
 def dictionary_of_article(article_link):
-   html_text = requests.get(article_link).text
-   while 'Error 503' in html_text:
-       time.sleep(2)
-       html_text = requests.get(article_link).text
-   soup = BeautifulSoup(html_text, 'lxml')
-   
-   dictionary_of_article = {}
-   
-   #DATA
-   
-   date_of_publication = soup.find('h2', class_='date-header').text
-   date = re.sub(r'(.*\,\s)(\d{1,2}\s)(.*)(\s\d{4})', r'\2\3\4', date_of_publication)
-  
-   lookup_table = {"stycznia": "01", "lutego": "02", "marca": "03", "kwietnia": "04", "maja": "05", "czerwca": "06", "lipca": "07", "sierpnia": "08", "września": "09", "października": "10", "listopada": "11", "grudnia": "12"}
-   s = date
-   for k, v in lookup_table.items():
-       s = s.replace(k, v)
-   
-   result = time.strptime(s, "%d %m %Y")
-   changed_date = datetime.fromtimestamp(mktime(result))   
-   new_date = format(changed_date.date())
-   
-   
-   try:
-       texts_of_article = soup.find_all('div', class_='post-body entry-content')
-      
-   except AttributeError:
-       pass 
-   except IndexError:   
-       pass
+    html_text = requests.get(article_link).text
+    while 'Error 503' in html_text:
+        time.sleep(2)
+        html_text = requests.get(article_link).text
+    soup = BeautifulSoup(html_text, 'lxml')
+ 
+    date_of_publication = soup.find('h2', class_='date-header').text
+    new_date = date_change_format_long(date_of_publication)
+    text_of_article = soup.find('div', class_='post-body entry-content')
+    article = text_of_article.text.strip().replace('\n', ' ').replace('\xa0', '')
+    title_of_article = soup.find('h3', class_='post-title entry-title').text.strip()
     
-   
-   for element in texts_of_article:
-        try:
-            article = element.text.strip().replace('\n', ' ')
-           
-            dictionary_of_article['Link'] = article_link
-            dictionary_of_article['Data publikacji'] = new_date
-            dictionary_of_article['Autor'] = 'Paweł Podlipniak'
-            
-            title_of_article = soup.find('h3', class_='post-title entry-title').text.strip()
-            dictionary_of_article['Tytuł artykułu'] = title_of_article
-            
-            dictionary_of_article['Tekst artykułu'] = article.replace('\xa0', '')
-
-        
-        except AttributeError:
-            pass 
-        except IndexError:   
-            pass
-      
-        try:
-
-            list_of_poems = [x.text for x in element.find_all('b')]  
-            titles_of_poems = [x.strip() for x in list_of_poems if x != '\xa0' and x != '' and x != '\n']
-            
-            dictionary_of_article['Tytuły wierszy'] = " | ".join(titles_of_poems).replace('\n', ' ')
-            
-            links_in_article = [x['href'] for x in element.find_all('a')]
-            dictionary_of_article['Linki_zewnętrzne'] = ' | '.join([x for x in links_in_article if not re.findall(r'blogspot|jpg', x)])
-               
-            
-            list_of_images = [x['src'] for x in element.find_all('img')]
-            if list_of_images != []:
-                dictionary_of_article['Zdjęcia/Grafika'] = 'TAK'
-                
-            
-            list_of_video = [x['src'] for x in element.find_all('iframe')]
-            if list_of_video != []:
-                dictionary_of_article['Filmy'] = 'TAK'
-                  
-        except AttributeError:
-            pass 
-        except IndexError:   
-            pass   
-        
-
-        all_results.append(dictionary_of_article)
+    try: 
+        list_of_poems = [x.text for x in text_of_article.find_all('b')]  
+        titles_of_poems = " | ".join([x.strip() for x in list_of_poems if x != '\xa0' and x != '' and x != '\n']).replace('\n', ' ')
+    except (AttributeError, KeyError, IndexError):
+        titles_of_poems = None
+    
+    try:
+        external_links = ' | '.join([x for x in [x['href'] for x in text_of_article.find_all('a')] if not re.findall(r'blogger|blogspot|komnen', x)])
+    except (AttributeError, KeyError, IndexError):
+        external_links = None
+         
+    try: 
+        photos_links = ' | '.join([x['src'] for x in text_of_article.find_all('img')])  
+    except (AttributeError, KeyError, IndexError):
+        photos_links = None
+         
+    dictionary_of_article = {'Link': article_link,
+                             'Data publikacji': new_date,
+                             'Autor': "Paweł Podlipniak",
+                             'Tytuł artykułu': title_of_article,
+                             "Tekst artykułu": article,
+                             'Tytuły wierszy': titles_of_poems,
+                             'Linki zewnętrzne': external_links,
+                             'Zdjęcia/Grafika': True if [x['src'] for x in text_of_article.find_all('img')] else False,
+                             'Filmy': True if [x['src'] for x in text_of_article.find_all('iframe')] else False,
+                             'Linki do zdjęć': photos_links
+                             }
+ 
+    all_results.append(dictionary_of_article)
 
 #%% main
 
 sitemap_links = komnem_kastamonu_web_scraping_sitemap('https://komnen-kastamonu.blogspot.com/sitemap.xml')
+
 articles_links = []
- 
 with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(get_article_pages, sitemap_links),total=len(sitemap_links)))
 
 all_results = []
-  
 with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(dictionary_of_article, articles_links),total=len(articles_links)))
+  
     
 with open(f'komnen_kastamonu_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
     json.dump(all_results, f)        
     
-
-df = pd.DataFrame(all_results)
-df = df.drop_duplicates()
-df["Data publikacji"] = pd.to_datetime(df["Data publikacji"])
+df = pd.DataFrame(all_results).drop_duplicates()
+df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
 df = df.sort_values('Data publikacji', ascending=False)
-df.to_excel(f"komnen_kastamonu_{datetime.today().date()}.xlsx", encoding='utf-8', index=False)   
-     
         
-       
-        
+with pd.ExcelWriter(f"komnen_kastamonu_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
+    writer.save()     
+   
+
+
+#%%Uploading files on Google Drive
+
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)   
+      
+upload_file_list = [f"komnen_kastamonu_{datetime.today().date()}.xlsx", f'komnen_kastamonu_{datetime.today().date()}.json']
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'parents': [{'id': '19t1szTXTCczteiKfF2ukYsuiWpDqyo8f'}]})  
+	gfile.SetContentFile(upload_file)
+	gfile.Upload()          
        
         
        
--- krytycznym_okiem.py
+++ krytycznym_okiem.py
@@ -9,7 +9,9 @@ from tqdm import tqdm  #licznik
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 import json
-
+from functions import date_change_format_short
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 
 #%%def
@@ -32,42 +34,70 @@ def dictionary_of_article(article_link):
         html_text = requests.get(article_link).text 
     soup = BeautifulSoup(html_text, 'lxml')
     
-    dictionary_of_article = {}
-    
     author = "Jarosław Czechowicz"
     title_of_article = soup.find('h3', class_='post-title entry-title').text.strip()
-    author_of_book = re.findall(r'(?<=[\"|\”]\s)(.*\s*\.*\s*?\.*?)', title_of_article)  
-    title_of_book = re.findall(r'[\"\„].*[\"\”](?=\s.*)', title_of_article)
     date_of_publication = soup.find('h2', class_='date-header').text
-    texts_of_article = soup.find_all('div', class_='post-body entry-content')
-    tags = soup.find_all('span', class_='post-labels')     
+    text_of_article = soup.find('div', class_='post-body entry-content')
+    article = text_of_article.text.strip().replace('\xa0', ' ')
+    tags_span = soup.find_all('span', class_='post-labels')     
+    tags = [tag.text.replace('Etykiety:', '').strip().replace('\n', ' ') for tag in tags_span][0]
+    
+    try:
+        author_of_book = re.findall(r'(?<=[\"|\”]\s)(.*\s*\.*\s*?\.*?)', title_of_article)[0] 
+    except IndexError:
+        author_of_book = None    
+        
+    try:
+        title_of_book = re.findall(r'[\"\„].*[\"\”](?=\s.*)', title_of_article)[0]
+    except IndexError:
+        title_of_book = None
+        
+    try:
+        title_of_review = re.findall(r'(?<=Tytuł recenzji:\s).*\s?.*?(?=\p{Lu}|\n)', article)[0].replace('\n', ' ').strip() 
+        
+    except (AttributeError, KeyError, IndexError):
+        title_of_review = None
+
+    try:
+        publication_of_book = date_change_format_short(re.findall(r'(?<=Data wydania:\s).*(?=\n)', article)[0])
+        year_of_publication = re.sub(r'(\d{4})(\-\d{2})(\-\d{2})', r'\1', publication_of_book)
+    except (AttributeError, KeyError, IndexError, ValueError):
+        year_of_publication = None
+        
+    try:
+        publisher = re.findall(r'(?<=Wydawca:\s).*\s?.*?(?=\n|D)', article)[0].replace('\n', ' ').strip()
+    except (AttributeError, KeyError, IndexError):
+        publisher = None
+        
+    try:
+        external_links = ' | '.join([x for x in [x['href'] for x in text_of_article.find_all('a')] if not re.findall(r'blogger|blogspot|komnen', x)])
+    except (AttributeError, KeyError, IndexError):
+        external_links = None
+         
+    try: 
+        photos_links = ' | '.join([x['src'] for x in text_of_article.find_all('img')])  
+    except (AttributeError, KeyError, IndexError):
+        photos_links = None
     
     
-    for element in texts_of_article:
-        try:
-            article = element.text.strip()
+    dictionary_of_article = {'Link': article_link,
+                             'Data publikacji': date_of_publication,
+                             'Autor': author,
+                             'Tytuł artykułu': title_of_article,
+                             'Autor książki': author_of_book,
+                             'Tytuł książki': title_of_book,
+                             'Tekst artykułu': article.replace('\n', ' '),
+                             'Odautorski tytuł recenzji': title_of_review if type(title_of_review) == str and len(title_of_review) < 60 else None,
+                             'Rok wydania': year_of_publication,
+                             'Wydawnictwo': publisher if type(publisher) == str and len(publisher) < 60 else None, 
+                             'Tagi': tags, 
+                             'Linki zewnętrzne': external_links,
+                             'Zdjęcia/Grafika': True if [x['src'] for x in text_of_article.find_all('img')] else False,
+                             'Filmy': True if [x['src'] for x in text_of_article.find_all('iframe')] else False,
+                             'Linki do zdjęć': photos_links
+                             }
             
-            dictionary_of_article['Link'] = article_link
-            dictionary_of_article['Autor'] = author
-            dictionary_of_article['Tytuł artykułu'] = title_of_article
-            dictionary_of_article['Data publikacji'] = date_of_publication
-           
-            dictionary_of_article['Autor książki'] = author_of_book[0]
-            dictionary_of_article['Tytuł książki'] = title_of_book[0]
-            dictionary_of_article['Tekst artykułu'] = article.replace('\n', ' ')
-            
-            dictionary_of_article['Odautorski tytuł recenzji'] = re.findall(r'(?<=Tytuł recenzji:\s).*\s?.*?(?=\p{Lu}|\n)', article)[0].replace('\n', ' ').strip() 
-            dictionary_of_article['Data wydania książki'] = re.findall(r'(?<=Data wydania:\s).*(?=\n)', article)[0]
-            dictionary_of_article['Wydawnictwo'] = re.findall(r'(?<=Wydawca:\s).*\s?.*?(?=\n|D)', article)[0].replace('\n', ' ').strip()
-            
-            dictionary_of_article['Tagi'] = [tag.text.replace('Etykiety:', '').strip().replace('\n', ' ') for tag in tags][0]
-            
-            
-        except AttributeError:
-            pass 
-        except IndexError:   
-            pass
-        all_results.append(dictionary_of_article)
+    all_results.append(dictionary_of_article)
         
 
 #%% main
@@ -78,7 +108,6 @@ with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(get_article_pages, sitemap_links),total=len(sitemap_links)))
    
 all_results = []
-
 with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(dictionary_of_article, articles_links),total=len(articles_links)))
 
@@ -87,11 +116,34 @@ with open(f'krytycznym_okiem_{datetime.today().date()}.json', 'w', encoding='utf
     json.dump(all_results, f)   
 
 
-df = pd.DataFrame(all_results)
-df = df.drop_duplicates()
-df["Data publikacji"] = pd.to_datetime(df["Data publikacji"])
+df = pd.DataFrame(all_results).drop_duplicates()
+df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
 df = df.sort_values('Data publikacji', ascending=False)
-df.to_excel(f"krytycznym_okiem_{datetime.today().date()}.xlsx", encoding='utf-8', index=False) 
+      
+with pd.ExcelWriter(f"krytycznym_okiem_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
+    writer.save()     
+   
+
+#%%Uploading files on Google Drive
+
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)   
+      
+upload_file_list = [f"krytycznym_okiem_{datetime.today().date()}.xlsx", f'krytycznym_okiem_{datetime.today().date()}.json']
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'parents': [{'id': '19t1szTXTCczteiKfF2ukYsuiWpDqyo8f'}]})  
+	gfile.SetContentFile(upload_file)
+	gfile.Upload()         
+
+
+
+
+
+
+
+
+
 
 
 
