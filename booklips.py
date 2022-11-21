5227917d71218efad2e9a067375cc45df029bd07
Basia <barbara.wachek@gmail.com>
2022-11-21 13:30:37 +0100

    2022-11-21

--- booklips.py
+++ booklips.py
@@ -29,7 +29,7 @@ def get_links_of_sitemap_links_posts(link):
     return all_articles_links_posts
  
 
-def dictionary_of_article(link):
+def dictionary_of_article_posts(link):
     #link = 'https://booklips.pl/newsy/zmarl-kevin-oneill-wspoltworca-serii-komiksowej-liga-niezwyklych-dzentelmenow/'
     #link = 'http://booklips.pl/czytelnia/opowiadania/nieznana-historia-milosna-pajaka-jednego-z-bohaterow-serii-kolory-zla-przeczytaj-opowiadanie-kryminalne-cytrusy-i-migdaly-malgorzaty-oliwii-sobczak/'
     #link = 'https://booklips.pl/recenzje/w-pulapce-bez-wyjscia-recenzja-ksiazki-ruiny-scotta-smitha/' #recenzja
@@ -239,20 +239,115 @@ def dictionary_of_article(link):
 
     all_results_posts.append(dictionary_of_article)
     
-    
+
+#re.search(r'^https:\/\/booklips\.pl\/(autorzy|wydawcy|ksiazki|katalog|komiksy|zasady-korzystania|reklama|regulamin-konkursow|regulamin-konkursow-na-facebooku-twitterze-instagramie|o-nas|redakcja)\/\w\/$', link):    
+
+
+
 def get_links_of_sitemap_links_pages(link):
+    #link = 'https://booklips.pl/sitemap.xml'
     html_text = requests.get(link).text
     soup = BeautifulSoup(html_text, 'lxml')
-    links = [x.text for x in soup.find_all('loc') if not re.findall(r'(https\:\/\/booklips\.pl\/sitemap-pt-)(post)(-\d{4}-\d{2}\.xml)', x.text) and not re.findall(r'https\:\/\/booklips\.pl\/sitemap-misc\.xml', x.text)]
-    
+    links = [x.text for x in soup.find_all('loc') if not re.findall(r'(https\:\/\/booklips\.pl\/sitemap)(-pt-post|-misc)(-\d{4}-\d{2})?(\.xml)', x.text)] 
+
     for link in tqdm(links):
-    
         html_text = requests.get(link).text
         soup = BeautifulSoup(html_text, 'lxml')
         articles_links = [x.text for x in soup.find_all('loc')]
         all_articles_links_pages.extend(articles_links)
-    
+            
     return all_articles_links_pages    
+
+def links_of_pages_without_unnecessaries(link):
+    #link = 'https://booklips.pl/autorzy/'
+    #link = 'https://booklips.pl/autorzy/a/'
+        
+    # if not re.findall(r'autorzy\/\w?\/?$', link):
+    #     all_articles_links_pages_without_unnecessaries.append(link)
+    
+    if not re.search(r'^https:\/\/booklips\.pl\/(autorzy|wydawcy|ksiazki|katalog|komiksy|zasady-korzystania|reklama|regulamin-konkursow|regulamin-konkursow-na-facebooku-twitterze-instagramie|o-nas|redakcja)\/\w?\d?\/?$', link):
+        all_articles_links_pages_without_unnecessaries.append(link)
+       
+    return all_articles_links_pages_without_unnecessaries
+
+
+
+def dictionary_of_article_pages(link):
+    
+    # link = 'https://booklips.pl/ksiazki/inna-dusza/'
+    link = 'https://booklips.pl/komiksy/moj-syn/'
+    link = 'https://booklips.pl/ksiazki/klub-julietty/'
+ 
+    html_text = requests.get(link).content
+    soup = BeautifulSoup(html_text, 'lxml')
+    
+        
+    title_of_article = soup.find('h1', class_='page-title entry-title')
+    if title_of_article:
+        title_of_article = title_of_article.text
+    else:
+        title_of_article = None
+        
+        
+    category = re.search(r'(?<=https?\:\/\/booklips\.pl\/)(\w*\-?\w*\-?\w*)(?=\/.*)', link).group(0)
+    content_of_article = soup.find('div', class_='entry')
+    
+    text_of_article = [x.text.replace('\n',' ') for x in content_of_article.find_all('p', class_=None)]
+    if text_of_article:
+        text_of_article = " ".join(text_of_article).strip()
+    else:
+        text_of_article = None
+          
+
+    external_links = [x for x in [x['href'] for x in content_of_article.find_all('a')] if not re.findall(r'(booklips)|(http://twitter.com/share)', x)]
+    if external_links != []:
+        external_links = ' | '.join(external_links)
+    else:
+        external_links = None
+        
+
+    photos_links = [x['src'] for x in content_of_article.find_all('img')]
+    if photos_links != []:
+        photos_links = ' | '.join(photos_links)
+    else:
+        photos_links = None
+             
+    
+
+    title_of_book = None
+    author_of_book = None
+    isbn = None
+    description_of_book = None
+    year_of_publication = None
+    
+    if text_of_article != None and (category == 'ksiazki' or category == 'komiksy'): 
+
+        if re.search(r'(?<=Scenariusz i rysunki:\s|Autor:\s|Scenariusz:\s)(\p{L}*\.?\s\p{L}*\.?\-?\s*\p{L}*\s*\p{L}*\s*\p{L}*\s*)(?=Tłumaczenie:)', text_of_article):
+            author_of_book = re.search(r'(?<=Scenariusz i rysunki:\s|Autor:\s)(\p{L}*\.?\s\p{L}*\.?\-?\s*\p{L}*\s*\p{L}*\s*\p{L}*\s*)(?=Tłumaczenie:)', text_of_article).group(0).strip()
+        title_of_book = title_of_article
+        if re.search(r'(?<=Opis:)(.*)(?=Materiały o .*)', text_of_article):
+            description_of_book = re.search(r'(?<=Opis:)(.*)(?=Materiały o .*)', text_of_article).group(0).strip()
+        if re.search(r'(?<=ISBN )[\d\-]*', text_of_article):
+            isbn = re.search(r'(?<=ISBN )[\d\-]*', text_of_article).group(0)
+        if re.search(r'(?<=Rok wydania:\s)\w*\s\d{4}', text_of_article):
+            year_of_publication = re.search(r'(?<=Rok wydania:\s)\w*\s\d{4}', text_of_article).group(0)
+        
+    dictionary_of_article = {'Link': link, 
+                             'Kategoria': category,
+                             'Tytuł artykułu': title_of_article,
+                             'Tekst artykułu': text_of_article,
+                             'Autor książki': author_of_book,
+                             'Tytuł książki': title_of_book, 
+                             'Rok i miesiąc wydania': year_of_publication,
+                             'Opis książki': description_of_book,
+                             'ISBN': isbn,
+                             'Linki zewnętrzne': external_links,
+                             'Zdjęcia/Grafika': True if [x['src'] for x in content_of_article.find_all('img')] else False,
+                             'Filmy': True if [x['src'] for x in content_of_article.find_all('iframe')] else False,
+                             'Linki do zdjęć': photos_links
+                             }
+
+    all_results_pages.append(dictionary_of_article)    
     
 #%% main
 sitemap_links = ['https://booklips.pl/sitemap.xml']   
@@ -300,12 +395,23 @@ with ThreadPoolExecutor() as excecutor:
 
 all_results_posts = []
 with ThreadPoolExecutor() as excecutor:
-    list(tqdm(excecutor.map(dictionary_of_article, all_articles_links_posts), total=len(all_articles_links_posts)))       
+    list(tqdm(excecutor.map(dictionary_of_article_posts, all_articles_links_posts), total=len(all_articles_links_posts)))       
 
 
 all_articles_links_pages = []
 with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(get_links_of_sitemap_links_pages, sitemap_links), total=len(sitemap_links)))  
+
+
+all_articles_links_pages_without_unnecessaries = []
+with ThreadPoolExecutor() as excecutor:
+    list(tqdm(excecutor.map(links_of_pages_without_unnecessaries, all_articles_links_pages), total=len(all_articles_links_pages)))  
+
+
+all_results_pages = []
+with ThreadPoolExecutor() as excecutor:
+    list(tqdm(excecutor.map(dictionary_of_article_pages, all_articles_links_pages_without_unnecessaries), total=len(all_articles_links_pages_without_unnecessaries)))       
+
 
 
 
@@ -316,9 +422,14 @@ with open(f'booklips_{datetime.today().date()}.json', 'w', encoding='utf-8') as 
 df_posts = pd.DataFrame(all_results_posts).drop_duplicates()
 df_posts["Data publikacji"] = pd.to_datetime(df_posts["Data publikacji"]).dt.date
 df_posts = df_posts.sort_values('Data publikacji', ascending=False)
+
+
+df_pages = pd.DataFrame(all_results_pages).drop_duplicates()
+
    
 with pd.ExcelWriter(f'booklips_{datetime.today().date()}.xlsx', engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
-    df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
+    df_posts.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
+    df_pages.to_excel(writer, 'Pages', index=False, encoding='utf-8')   
     writer.save()     
    
 
@@ -338,14 +449,13 @@ for upload_file in upload_file_list:
 #NOTATKI
 
 #Do analizy częć ksiażki i autorzy - inna struktura artykułów + wiecej informacji - nie mają dat publikacji - przeanalizować wszystklie linki, które zawierą element page
-#Autor w recenzji czasem kursywą! 
 # POdzielić linki z post i page na dwa arkusze i osobno zeskrobywać 
 
 
 #2022-11-17
-#Zeskrobać częsc z pages (ksiazki, komiksy, autorzy). Najpierw usunac z all_articles_links_posts zbędne linki np. https://booklips.pl/ksiazki/m/ itp. 
+#Poprawić dane w pages w kat. ksiazki/komiksy
 
 #Dodać sleep, bo czasami jest ConnectionError
-#Usprawnić pobieranie tytułów i autorów książek z kategorii Recenzja
+
 
 
