fed88e826e46738d012e39570e5e384dbee1fce1
Basia <barbara.wachek@gmail.com>
2022-11-29 15:21:57 +0100

    2022-11-29

---
+++ culture_pl.py
@@ -0,0 +1,23 @@
+#%%import
+# from __future__ import unicode_literals
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+# import regex as re
+# from tqdm import tqdm  #licznik
+# from concurrent.futures import ThreadPoolExecutor
+# from datetime import datetime
+# import json
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+
+# from functions import date_change_format_short
+
+
+
+
+#%% def
+
+import requests
+
+sitemap_link = 'https://culture.pl/pl/sitemap.xml'
