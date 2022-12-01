6fb94a448ed40266d97bfd0b444e96aade08cab4
Basia <barbara.wachek@gmail.com>
2022-12-01 14:37:59 +0100

    2022-12-01

--- tomasz_białkowski.py
+++ tomasz_białkowski.py
@@ -9,7 +9,7 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from time import mktime
 import json
-
+from functions import date_change_format_long
 
 #%% def
 def tomaszbialkowski_web_scraping_sitemap(sitemap):
@@ -30,89 +30,59 @@ def dictionary_of_article(article_link):
         time.sleep(2)
         html_text = requests.get(article_link).text
     soup = BeautifulSoup(html_text, 'lxml')
-    
-    dictionary_of_article = {}
-    
-    #DATA
-    
-    date_of_publication = soup.find('h2', class_='date-header').text
-    date = re.sub(r'(.*\,\s)(\d{1,2}\s)(.*)(\s\d{4})', r'\2\3\4', date_of_publication)
- 
-    lookup_table = {"stycznia": "01", "lutego": "02", "marca": "03", "kwietnia": "04", "maja": "05", "czerwca": "06", "lipca": "07", "sierpnia": "08", "września": "09", "października": "10", "listopada": "11", "grudnia": "12"}
-    s = date
-    for k, v in lookup_table.items():
-        s = s.replace(k, v)
-    
-    result = time.strptime(s, "%d %m %Y")
-    changed_date = datetime.fromtimestamp(mktime(result))   
-    new_date = format(changed_date.date())
-    
-    try:
-        texts_of_article = soup.find_all('div', class_='post-body entry-content')
-        tags_span = soup.find_all('span', class_='post-labels')
-        tags = [tag.text for tag in tags_span][0].strip().split('\n')
-  
-    except AttributeError:
-        pass 
-    except IndexError:   
-        pass
-    
-    
-    for element in texts_of_article:
-        try:
-            article = element.text.strip().replace('\n', ' ')
-           
-            dictionary_of_article['Link'] = article_link
-            dictionary_of_article['Autor'] = soup.find('span', attrs={'itemprop':'name'}).text
-            
-            dictionary_of_article['Data publikacji'] = new_date
-            
-        
-            title_of_article = soup.find('h3', class_='post-title entry-title').text.strip()
-            dictionary_of_article['Tytuł artykułu'] = title_of_article
-            
-            
-            dictionary_of_article['Tekst artykułu'] = article
-            dictionary_of_article['Tagi'] = '| '.join(tags[1:]).replace(',', ' ')
-            
-        except AttributeError:
-            pass 
-        except IndexError:   
-            pass
-    
-        try:
-            
-            links_in_article = [x['href'] for x in element.find_all('a')]
-            dictionary_of_article['Linki_zewnętrzne'] = ' | '.join([x for x in links_in_article if not re.findall(r'blogspot|jpg', x)])
-            
-            list_of_images = [x['src'] for x in element.find_all('img')]
-            if list_of_images != []:
-                dictionary_of_article['Zdjęcia/Grafika'] = 'TAK'
-                
-            
-            list_of_video = [x['src'] for x in element.find_all('iframe')]
-            if list_of_video != []:
-                dictionary_of_article['Filmy'] = 'TAK'
-            
-        except AttributeError:
-            pass 
-        except IndexError:   
-            pass   
-        except KeyError:
-            pass
-        
-        try:
-            links_of_images = [x['src'] for x in element.find_all('img')]
-            dictionary_of_article['Linki do zdjęć'] = ' | '.join(links_of_images)
-                    
-        except AttributeError:
-            pass 
-        except IndexError:   
-            pass
-        except KeyError:
-            pass        
+     
+    date_of_publication = date_change_format_long(soup.find('h2', class_='date-header').text)
 
-        all_results.append(dictionary_of_article)
+
+    content_of_article = soup.find('div', class_='post-body entry-content')
+    
+    tags_span = soup.find('span', class_='post-labels').findChildren('a')
+    if tags_span:
+        tags = " | ".join([tag.text for tag in tags_span])
+    else:
+        tags = None
+  
+    author = soup.find('span', attrs={'itemprop':'name'})
+    if author:
+        author = author.text
+    else:
+        author = None
+
+
+    text_of_article = content_of_article.text.strip().replace('\n', '')
+ 
+    title_of_article = soup.find('h3', class_='post-title entry-title')
+    if title_of_article:
+        title_of_article = title_of_article.text.strip()     
+    else:
+        title_of_article = None
+
+    try:
+        external_links = ' | '.join([x for x in [x['href'] for x in content_of_article.find_all('a')] if not re.findall(r'(blogspot)|(jpg)', x)])
+    except KeyError:
+        external_links = None
+        
+    try:
+        photos_links = ' | '.join([x['src'] for x in content_of_article.find_all('img')])
+    except KeyError:
+        photos_links = None
+        
+
+
+
+    dictionary_of_article = {'Link': article_link,
+                             'Data publikacji': date_of_publication,
+                             'Autor': author,
+                             'Tytuł artykułu': title_of_article,
+                             'Tekst artykułu': text_of_article,
+                             'Tagi': tags,
+                             'Linki zewnętrzne': external_links,
+                             'Linki do zdjęć': photos_links
+                             }
+
+    all_results.append(dictionary_of_article)
+    
+    
     
 #%% main
 
@@ -123,22 +93,21 @@ with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(get_article_pages, sitemap_links),total=len(sitemap_links)))
     
 all_results = []
-
 with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(dictionary_of_article, articles_links),total=len(articles_links)))
+
+
 
 with open(f'tomasz_bialkowski_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
     json.dump(all_results, f)   
 
     
-df = pd.DataFrame(all_results)
-df = df.drop_duplicates()
-df["Data publikacji"] = pd.to_datetime(df["Data publikacji"])
+df = pd.DataFrame(all_results).drop_duplicates()
 df = df.sort_values('Data publikacji', ascending=False)
-df.to_excel(f"tomasz_bialkowski_{datetime.today().date()}.xlsx", encoding='utf-8', index=False)   
 
     
-    
+with pd.ExcelWriter(f"tomasz_bialkowski_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df.to_excel(writer, 'Posty', index=False)    
     
     
     
