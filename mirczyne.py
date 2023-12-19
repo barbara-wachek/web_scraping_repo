d2839751d030d7c984e3aad95cc720f9bfdbfa38
Nikodem Wołczuk <nikodem.wolczuk@ibl.waw.pl>
2023-12-19 15:40:40 +0100

    mirczyne

---
+++ mirczyne.py
@@ -0,0 +1,115 @@
+#%%import
+from __future__ import unicode_literals
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import regex as re
+import time
+from tqdm import tqdm  #licznik
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+import json
+from functions import date_change_format_long
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from selenium import webdriver
+
+#%%def
+def process_sitemap(sitemap):
+    html_text_sitemap = requests.get(sitemap).text
+    soup = BeautifulSoup(html_text_sitemap, 'lxml')
+    links = [e.text for e in soup.find_all('loc')]
+    return links
+
+def dictionary_of_article(article_link):
+    response = requests.get(article_link)
+    if response.status_code != 200:
+        return
+
+    html_text = response.text
+    soup = BeautifulSoup(html_text, 'lxml')
+    
+    if soup.find('div', class_='post hentry uncustomized-post-template'):
+        article_body = soup.find('div', class_='post hentry uncustomized-post-template')
+    else: return
+    
+    #date
+    date_of_publication = article_body.find('a', class_='timestamp-link').find('abbr')['title']
+    date_of_publication = datetime.fromisoformat(date_of_publication)
+    date_of_publication = date_of_publication.strftime('%Y-%m-%d')
+    
+    #title
+    if title_of_article := article_body.find('h3', class_="post-title entry-title"):
+        title_of_article = title_of_article.text.strip()
+    else: title_of_article = None
+    
+    #author
+    if author := article_body.find('a', class_='g-profile'):
+        author = author.text.strip()
+    else: author = None
+    
+    #etykiety
+    tags = article_body.find_all('a', {'rel': 'tag'})
+    tags = ' | '.join([e.text.strip() for e in tags])
+    
+    
+    
+    if art_content := article_body.find('div', class_='post-body entry-content'):
+        text_of_article = art_content
+        article = text_of_article.text.strip().replace('\n', ' ')
+    else:
+        text_of_article, article = None, None 
+    
+    
+    try:
+        external_links = ' | '.join([x for x in [x['href'] for x in text_of_article.find_all('a')] if not re.findall(r'blogger|blogspot|intimathule', x)])
+    except (AttributeError, KeyError, IndexError):
+        external_links = None
+        
+    try: 
+        photos_links = ' | '.join([x['src'] for x in text_of_article.find_all('img')])  
+    except (AttributeError, KeyError, IndexError):
+        photos_links = None
+        
+
+    dictionary_of_article = {'Link': article_link,
+                             'Data publikacji': date_of_publication,
+                             'Autor': author,
+                             'Tytuł artykułu': title_of_article,
+                             'Etykiety': tags,
+                             'Tekst artykułu': article,
+                             'Linki zewnętrzne': external_links,
+                             'Linki do zdjęć': photos_links,
+                             }
+    all_results.append(dictionary_of_article)
+
+
+#%%
+sitemap_url = 'https://mirczyne.blogspot.com/sitemap.xml'
+articles_links = process_sitemap(sitemap_url)
+
+all_results = []
+with ThreadPoolExecutor() as excecutor:
+    list(tqdm(excecutor.map(dictionary_of_article, articles_links),total=len(articles_links)))
+
+with open(f'data/mirczyne_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
+    json.dump(all_results, f, ensure_ascii=False)        
+ 
+df = pd.DataFrame(all_results).drop_duplicates()
+df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
+df = df.sort_values('Data publikacji', ascending=False)
+   
+with pd.ExcelWriter(f"data/mirczyne_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
+    writer.save()   
+
+#%%Uploading files on Google Drive
+
+gauth = GoogleAuth()      
+drive = GoogleDrive(gauth)   
+      
+upload_file_list = [f"data/mirczyne_{datetime.today().date()}.xlsx", f'data/mirczyne_{datetime.today().date()}.json']
+for upload_file in upload_file_list:
+ 	gfile = drive.CreateFile({'parents': [{'id': '19t1szTXTCczteiKfF2ukYsuiWpDqyo8f'}]})  
+ 	gfile.SetContentFile(upload_file)
+ 	gfile.Upload()  
