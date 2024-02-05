02814dcbb4ac3c4f6b579d31647f737aff794b29
Cezary Rosiński <cezary.rosinski@gmail.com>
2024-02-05 17:10:32 +0100

    zeszytypoetyckie done

--- zeszytypoetyckie.py
+++ zeszytypoetyckie.py
@@ -29,35 +29,42 @@ def dictionary_of_article(link):
     html_text = requests.get(link).text
     soup = BeautifulSoup(html_text, 'html.parser')
     
-    date_of_publication = soup.find('time')['datetime'][:10]
+    date_of_publication = None
    
-    content_of_article = soup.find('div', class_='entry-content')
+    content_of_article = soup.find('td', {'valign': 'top'})
     
-    tags = '|'.join([e.text for e in soup.find_all('a', rel='tag')][1:])
+    tags = None
   
-    author = soup.find('span', class_='author vcard').find('a')
-    if author:
-        author = author.text
+    author = soup.find('span', {'style': 'color:#000000;'})
+    if author and author.find('strong'):
+        author = author.find('strong').text
     else: author = None
-
-    text_of_article = content_of_article.text.strip().replace('\n', '')
+    
+    if content_of_article:
+        text_of_article = content_of_article.text.strip().replace('\n', '')
+    else:
+        text_of_article = None
  
-    title_of_article = soup.find('h1', class_='entry-title')
+    title_of_article = soup.find('td', class_='contentheading')
     if title_of_article:
         title_of_article = title_of_article.text.strip()     
     else:
         title_of_article = None
-
-    try:
-        external_links = ' | '.join([e.find('a')['href'] for e in content_of_article.find_all('p') if e.find('a')])
-    except KeyError:
-        external_links = None
+    
+    if content_of_article:
+        try:
+            external_links = ' | '.join([e.find('a')['href'] for e in content_of_article.find_all('p') if e.find('a') and 'zeszytypoetyckie.pl' not in e.find('a')['href']])
+        except KeyError:
+            external_links = None
         
-    try:
-        photos_links = ' | '.join([x['src'] for x in content_of_article.find_all('img')])
-    except KeyError:
+        try:
+            photos_links = ' | '.join([x['src'] for x in content_of_article.find_all('input', {'type': 'image'})])
+        except KeyError:
+            photos_links = None
+    else:
+        external_links = None
         photos_links = None
-
+        
     dictionary_of_article = {'Link': link,
                              'Data publikacji': date_of_publication,
                              'Autor': author,
@@ -71,7 +78,7 @@ def dictionary_of_article(link):
     all_results.append(dictionary_of_article)
     
 #%% main
-articles_links = get_links_of_sitemap('https://zanoty.wordpress.com/sitemap.xml')
+articles_links = get_links_of_sitemap('https://zeszytypoetyckie.pl/mapa-strony')
 
 all_results = []
 with ThreadPoolExecutor() as excecutor:
