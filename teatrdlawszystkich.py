37fae5f9d100b25e212aaecc80b7ca6760220ae7
Cezary Rosiński <cezary.rosinski@gmail.com>
2024-03-07 17:42:06 +0100

    teatrdlawszystkich harvested

---
+++ teatrdlawszystkich.py
@@ -0,0 +1,134 @@
+#%%import
+from __future__ import unicode_literals
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import regex as re
+import time
+from time import mktime
+from tqdm import tqdm  #licznik
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+import json
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from functions import date_change_format_short
+
+
+#%% def
+def get_links_of_sitemap(sitemap_link):
+    # sitemap_link = 'https://teatrdlawszystkich.eu/wp-sitemap.xml'
+    html_text = requests.get(sitemap_link).text
+    soup = BeautifulSoup(html_text, "html.parser")
+    links = [x.text for x in soup.find_all('loc') if '-posts-post-' in x.text]
+    art_links = []
+    for link in tqdm(links):
+        html_text = requests.get(link).text
+        soup = BeautifulSoup(html_text, "html.parser")
+        site_links = [e.text for e in soup.find_all('loc')]
+        art_links.extend(site_links)  
+    return art_links
+
+def dictionary_of_article(link):
+    # link = 'https://teatrdlawszystkich.eu/dziady-rehabilitacja/'
+
+    html_text = requests.get(link).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    date_of_publication = date_change_format_short(soup.find('span', class_='posted-on').text)
+   
+    content_of_article = soup.find('div', class_='entry-content')
+    
+    tags = soup.find_all('a', rel='tag')
+    if tags:
+        tags = ' | '.join([e.text.strip() for e in tags][1:])
+    else: tags = None
+    
+    category = soup.find_all('a', rel='category tag')
+    if category:
+        category = ' | '.join([e.text.strip() for e in category])
+    else: category = None
+    
+    author = soup.find('div', class_='profile-info').find('h2')
+    if author:
+        author = author.text
+    else:
+        author = None
+    
+    text_of_article = '\n'.join([e.text.strip().replace('\n', '') for e in content_of_article.find_all('p')])
+    
+    title_of_article = soup.find('h1', class_='entry-title') 
+    if title_of_article:
+        title_of_article = title_of_article.text.strip()     
+    else:
+        title_of_article = None
+
+    try:
+        external_links = ' | '.join(set([el['href'] for sub in [e.find_all('a') for e in content_of_article.find_all('p')] for el in sub if el.has_attr('href') and 'teatrdlawszystkich' not in el['href']]))
+    except KeyError:
+        external_links = None
+        
+    try:
+        photos_links = ' | '.join([x['src'] for x in content_of_article.find_all('img')])
+        if not photos_links:
+            photos_links = ' | '.join([x['src'] for x in soup.find('div', 'entry-img').find_all('img')])
+    except (AttributeError, KeyError):
+        photos_links = None
+
+    dictionary_of_article = {'Link': link,
+                             'Data publikacji': date_of_publication,
+                             'Autor': author,
+                             'Tagi': tags,
+                             'Kategoria': category,
+                             'Tytuł artykułu': title_of_article,
+                             'Tekst artykułu': text_of_article,
+                             'Linki zewnętrzne': external_links,
+                             'Linki do zdjęć': photos_links,
+                             'Filmy': True if [x['src'] for x in content_of_article.find_all('iframe')] else False}
+
+    all_results.append(dictionary_of_article)
+    
+#%% main
+articles_links = get_links_of_sitemap('https://teatrdlawszystkich.eu/wp-sitemap.xml')
+
+all_results = []
+with ThreadPoolExecutor() as excecutor:
+    list(tqdm(excecutor.map(dictionary_of_article, articles_links),total=len(articles_links)))   
+
+with open(f'data/teatrdlawszystkich_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
+    json.dump(all_results, f, ensure_ascii=False, default=str) 
+
+df = pd.DataFrame(all_results)
+df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
+df = df.sort_values('Data publikacji', ascending=False)
+
+with pd.ExcelWriter(f"data/teatrdlawszystkich_{datetime.today().date()}.xlsx", engine='xlsxwriter', engine_kwargs={'options': {'strings_to_urls': False}}) as writer:    
+    df.to_excel(writer, 'Posts', index=False)
+
+#%%Uploading files on Google Drive
+
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)   
+      
+upload_file_list = [f"data/teatrdlawszystkich_{datetime.today().date()}.xlsx", f'data/teatrdlawszystkich_{datetime.today().date()}.json']
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'title': upload_file.replace('data/', ''), 'parents': [{'id': '19t1szTXTCczteiKfF2ukYsuiWpDqyo8f'}]})
+	gfile.SetContentFile(upload_file)
+	gfile.Upload()  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
