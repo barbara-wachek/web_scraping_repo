22189c26c46ab110c7fd31b8cf0df7743fd9c5e4
Cezary Rosiński <cezary.rosinski@gmail.com>
2024-05-28 21:18:00 +0200

    instytut ksiazki started

---
+++ instytutksiazki.py
@@ -0,0 +1,6 @@
+#schemat 1:
+"https://instytutksiazki.pl/literatura,8,nowosci-wydawnicze,21.html"
+"https://instytutksiazki.pl/literatura,8,recenzje,25.html"
+#schemat 2:
+"https://instytutksiazki.pl/literatura,8,nocny-stolik,67.html"
+"https://instytutksiazki.pl/literatura,8,slowo-od-tlumacza,68.html"
