af4061c1b1c60dac4902e086a48b8c88dbbfe3f9
Barbara Wachek <barbara.wachek@gmail.com>
2024-09-12 11:35:41 +0200

    2024-09-12

--- dziadzkielczowa.py
+++ dziadzkielczowa.py
@@ -10,8 +10,6 @@ from tqdm import tqdm  #licznik
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 import json
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 
 #%% def
 def get_articles_links(sitemap):
@@ -20,79 +18,30 @@ def get_articles_links(sitemap):
     links = [e.text for e in soup.find_all('loc')]
     return links
 
-
 def dictionary_of_article(article_link):
-    article_link = 'https://dziad-z-kielczowa.blogspot.com/2021/03/wirydarz-dla-przyjaciela-3-swiadkowie.html'
     html_text = requests.get(article_link).text
     while 'Error 503' in html_text:
         time.sleep(2)
         html_text = requests.get(article_link).text 
-    soup = BeautifulSoup(html_text, 'lxml')
+    soup = BeautifulSoup(html_text, 'html.parser')
     
     author = "Radosław Wiśniewski"
     title_of_article = soup.find('h3', class_='post-title entry-title').text.strip()
-    date_of_publication = soup.find('time', class_='published').text
+    date_of_publication = soup.find('time', class_='published')['datetime']
+    date_of_publication = re.findall(r'^([\d-]*)(?=T)', date_of_publication)[0]
     
-    
-    date_of_publication = soup.find('time').text
-    
-    #spróbować wyjąc name datetime albo przekszalcic text regeksem
-    
-    
-    
-    
-    
-    
-    
-    date_of_publication = re.sub(r'^.* \d{1,2}, \d{4}$', , date_of_publication)
-    
-    re.sub(pattern, repl, string, count=0, flags=0)
-    
-    
-    
-    text_of_article = soup.find('div', class_='post-body entry-content')
-    article = text_of_article.text.strip().replace('\xa0', ' ')
-    tags_span = soup.find_all('span', class_='post-labels')     
-    tags = [tag.text.replace('Etykiety:', '').strip().replace('\n', ' ') for tag in tags_span][0]
-    
-    
-    
-    
-    
-    try:
-        author_of_book = re.findall(r'(?<=[\"|\”]\s)(.*\s*\.*\s*?\.*?)', title_of_article)[0] 
-    except IndexError:
-        author_of_book = None    
-        
-    try:
-        title_of_book = re.findall(r'[\"\„].*[\"\”](?=\s.*)', title_of_article)[0]
-    except IndexError:
-        title_of_book = None
-        
-    try:
-        title_of_review = re.findall(r'(?<=Tytuł recenzji:\s).*\s?.*?(?=\p{Lu}|\n)', article)[0].replace('\n', ' ').strip() 
-        
-    except (AttributeError, KeyError, IndexError):
-        title_of_review = None
+     
+    article = soup.find('div', class_='post-body entry-content float-container')
+    text_of_article = article.text
 
-    try:
-        publication_of_book = date_change_format_short(re.findall(r'(?<=Data wydania:\s).*(?=\n)', article)[0])
-        year_of_publication = re.sub(r'(\d{4})(\-\d{2})(\-\d{2})', r'\1', publication_of_book)
-    except (AttributeError, KeyError, IndexError, ValueError):
-        year_of_publication = None
         
     try:
-        publisher = re.findall(r'(?<=Wydawca:\s).*\s?.*?(?=\n|D)', article)[0].replace('\n', ' ').strip()
-    except (AttributeError, KeyError, IndexError):
-        publisher = None
-        
-    try:
-        external_links = ' | '.join([x for x in [x['href'] for x in text_of_article.find_all('a')] if not re.findall(r'blogger|blogspot|krytycznymokiem', x)])
+        external_links = ' | '.join([x for x in [x['href'] for x in article.find_all('a')] if not re.findall(r'blogger|blogspot|dziadzkielczowa', x)])
     except (AttributeError, KeyError, IndexError):
         external_links = None
          
     try: 
-        photos_links = ' | '.join([x['src'] for x in text_of_article.find_all('img')])  
+        photos_links = ' | '.join([x['src'] for x in article.find_all('img')])  
     except (AttributeError, KeyError, IndexError):
         photos_links = None
     
@@ -101,21 +50,13 @@ def dictionary_of_article(article_link):
                              'Data publikacji': date_of_publication,
                              'Autor': author,
                              'Tytuł artykułu': title_of_article,
-                             'Autor książki': author_of_book,
-                             'Tytuł książki': title_of_book,
-                             'Tekst artykułu': article.replace('\n', ' '),
-                             'Odautorski tytuł recenzji': title_of_review if type(title_of_review) == str and len(title_of_review) < 60 else None,
-                             'Rok wydania': year_of_publication,
-                             'Wydawnictwo': publisher if type(publisher) == str and len(publisher) < 60 else None, 
-                             'Tagi': tags, 
-                             'Linki zewnętrzne': external_links,
-                             'Zdjęcia/Grafika': True if [x['src'] for x in text_of_article.find_all('img')] else False,
-                             'Filmy': True if [x['src'] for x in text_of_article.find_all('iframe')] else False,
+                             'Tekst artykułu': text_of_article,
+                             'Linki zewnętrzne': False if external_links == '' else external_links,
+                             'Zdjęcia/Grafika': True if [x['src'] for x in article.find_all('img')] else False,
                              'Linki do zdjęć': photos_links
                              }
             
     all_results.append(dictionary_of_article)
-
 
 
 
@@ -128,7 +69,7 @@ with ThreadPoolExecutor() as excecutor:
     list(tqdm(excecutor.map(dictionary_of_article, articles_links),total=len(articles_links)))
 
     
-with open(f'dziadzkielczowa_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
+with open(f'data\\dziadzkielczowa_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
     json.dump(all_results, f, ensure_ascii=False)   
 
 
@@ -136,21 +77,11 @@ df = pd.DataFrame(all_results).drop_duplicates()
 df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
 df = df.sort_values('Data publikacji', ascending=False)
       
-with pd.ExcelWriter(f"dziadzkielczowa__{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+with pd.ExcelWriter(f"data\\dziadzkielczowa_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
     df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
     writer.save()     
    
     
-   
-    
 
-#%%Uploading files on Google Drive
 
-gauth = GoogleAuth()           
-drive = GoogleDrive(gauth)   
-      
-upload_file_list = [f"krytycznym_okiem_{datetime.today().date()}.xlsx", f'krytycznym_okiem_{datetime.today().date()}.json']
-for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'parents': [{'id': '19t1szTXTCczteiKfF2ukYsuiWpDqyo8f'}]})  
-	gfile.SetContentFile(upload_file)
-	gfile.Upload()         
+
