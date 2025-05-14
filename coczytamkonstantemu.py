55fc43891d6966a17af78c7ac6a617de9ff7b748
Basia <barbara.wachek@gmail.com>
2025-05-14 11:48:35 +0200

    coczytamkonstantemu

---
+++ coczytamkonstantemu.py
@@ -0,0 +1,144 @@
+#%% import 
+from __future__ import unicode_literals
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import regex as re
+import time
+from tqdm import tqdm  #licznik
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+import json
+
+
+#%% def    
+
+def get_article_links(sitemap_link):
+    html_text = requests.get(sitemap_link).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    links = [e.text for e in soup.find_all('loc')]
+    return links
+
+def dictionary_of_article(article_link):
+    # article_link = 'http://www.coczytamkonstantemu.pl/2021/08/mapa-mordercy-tekst-i-ilustracje-jakob.html'
+    # article_link = 'http://www.coczytamkonstantemu.pl/2020/03/obce-swiadomosci-seria-kajtek-i-koko-w.html'
+    
+    
+    html_text = requests.get(article_link).text
+    while 'Error 503' in html_text:
+        time.sleep(2)
+        html_text = requests.get(article_link).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    
+    author = 'Bartosz Wokan'
+
+    try:
+        title_of_article = soup.find('h2', class_='post-title entry-title').find('a').text
+    except: 
+        title_of_article = None
+        
+        
+    try:
+        date_of_publication = soup.find('span', class_='published updated')['title'][:10]
+    except:
+        date_of_publication = None
+
+    
+    try:
+        tags = " | ".join([x.text for x in soup.find('span', class_='cat-linksnbt').find_all('a')])
+    except:
+        tags = None
+    
+    try:
+        title_of_book = " | ".join(re.findall(r'[„"](.*?)["”]', title_of_article))
+    except:
+        title_of_book = None
+        
+    try: 
+        author_of_book = " | ".join(re.findall(r'([A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)', title_of_article))
+    except:
+        author_of_book = None
+    
+    article = soup.find('div', class_='post-body entry-content')
+    
+    if article: 
+        text_of_article = " ".join([x.text for x in article.find_all(['div', 'p'], class_='MsoNormal')]).replace('\n', ' ').replace('  ', ' ').strip()
+    else:
+        text_of_article = None
+    
+    try:
+        external_links = ' | '.join([x for x in [x['href'] for x in article.find_all('a')] if not re.findall(r'blogger|blogspot|coczytamkonstantemu', x)])
+    except (AttributeError, KeyError, IndexError):
+        external_links = None
+        
+    try: 
+        photos_links = ' | '.join([x['src'] for x in article.find_all('img')])  
+    except (AttributeError, KeyError, IndexError):
+        photos_links = None
+
+    dictionary_of_article = {'Link': article_link,
+                             'Data publikacji': date_of_publication,
+                             'Autor': author,
+                             'Tytuł artykułu': title_of_article,
+                             'Autor książki': author_of_book,
+                             'Tytuł książki': title_of_book, 
+                             'Tekst artykułu': text_of_article,
+                             'Tagi': tags,
+                             'Linki zewnętrzne': external_links,
+                             'Zdjęcia/Grafika': True if [x['src'] for x in article.find_all('img')] else False,
+                             'Filmy': True if [x['src'] for x in article.find_all('iframe')] else False,
+                             'Linki do zdjęć': photos_links}
+        
+
+    all_results.append(dictionary_of_article)
+    
+    
+ 
+#%% main
+article_links = get_article_links('http://www.coczytamkonstantemu.pl/sitemap.xml')
+   
+all_results = []
+with ThreadPoolExecutor() as excecutor:
+    list(tqdm(excecutor.map(dictionary_of_article, article_links),total=len(article_links)))
+   
+
+df = pd.DataFrame(all_results).drop_duplicates()
+df["Data publikacji"] = pd.to_datetime(df["Data publikacji"]).dt.date
+df = df.sort_values('Data publikacji', ascending=True)
+
+
+with open(f'coczytamkonstantemu_{datetime.today().date()}.json', 'w', encoding='utf-8') as f:
+    json.dump(all_results, f, ensure_ascii=False)    
+
+with pd.ExcelWriter(f"coczytamkonstantemu_{datetime.today().date()}.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
+    writer.save()     
+   
+
+
+
+   
+   
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
