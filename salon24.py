1b02bd9a8bea8b785bb650f402bd290141eab3a1
Basia <barbara.wachek@gmail.com>
2026-02-05 14:06:59 +0100

    zmiany

--- salon24.py
+++ salon24.py
@@ -15,64 +15,45 @@ import time
 
 def dictionary_of_article(article_link):  
     
-    article_link = 'https://www.salon24.pl/u/wspomnienia/859744,epilog-raz-jeszcze'
+    #article_link = 'https://www.salon24.pl/u/wspomnienia/859744,epilog-raz-jeszcze'
+    #article_link = 'https://www.salon24.pl/u/wspomnienia/65773,interludium'
 
- 
     html_text = requests.get(article_link).text
     soup = BeautifulSoup(html_text, 'html.parser')
     
     
-    try:
-        header = soup.find('div', class_=" article-header--bloger")
-    except:
-        header = None
-
     
     try:
         title_of_article = soup.find('div', class_='article-header--content').h1.text
     except:
         title_of_article = None
   
-
-    author = 'Zofia Pawłowska'
         
     try:
-        author = soup.find('div', class_='article-header--info').find('a', class_="nick")
+        author = soup.find('div', class_='article-header--info').find('a', class_="nick").text
+    except:
+        author = None
     
         
     try:
-        tags = " | ".join([x.text for x in header.find('span', class_="tags-links").find_all('a')])
-      
+        tags = " | ".join([x.text for x in soup.find('div', class_="article-header--info").find_all('a', class_='button label')])
     except:
         tags = None  
          
         
     try:
-        date_of_publication = soup.find('time').get('datetime')[:10]
+        date_of_publication = soup.find('div', class_='article-meta article-meta--bloger').find('span', class_='label label--ago').get('data-date')[:10]
     except:
         date_of_publication = None    
         
 
     article = soup.find('div', class_='article-content')
     
-    try:
-        text_of_article = " ".join([x.text for x in article.find_all('p')])
-    except:
-        text_of_article = None
-    
-
-    
-        
-   
-    try:
-        tags = " | ".join([x.text for x in soup.find('span', class_="tags-links").find_all('a')])
-   
-    except:
-        tags = None
+    text_of_article = article.text.replace('\n', '')
     
    
     try:
-        external_links = ' | '.join([x for x in [x['href'] for x in article.find_all('a')] if not re.findall(r'jaroslaw', x)])
+        external_links = ' | '.join([x for x in [x['href'] for x in article.find_all('a')] if not re.findall(r'salon24', x)])
     except (AttributeError, KeyError, IndexError):
         external_links = None
         
@@ -100,16 +81,14 @@ for number in range(1,8):
     link = f'https://www.salon24.pl/u/wspomnienia/posts/0,{str(number)},wszystkie'
     subpages_links.append(link)
     
-articles_links = []
+article_links = []
 
 for link in subpages_links:
     html_text = requests.get(link).text
     soup = BeautifulSoup(html_text, 'html.parser')
     
     links = ["https:" + x.a['href'] for x in soup.find_all('h3', class_='title')]
-    articles_links.extend(links)
-
-
+    article_links.extend(links)
 
 
 
@@ -121,9 +100,9 @@ with ThreadPoolExecutor() as excecutor:
 df = pd.DataFrame(all_results).drop_duplicates()
 df = df.sort_values('Data publikacji', ascending=True)
 
-df.to_json(f'data/jaroslawpluciennik_{datetime.today().date()}.json', orient='records', force_ascii=False, indent=2)
+df.to_json(f'data/salon24_{datetime.today().date()}.json', orient='records', force_ascii=False, indent=2)
 
-with pd.ExcelWriter(f"data/jaroslawpluciennik_{datetime.today().date()}.xlsx", engine='xlsxwriter') as writer:    
+with pd.ExcelWriter(f"data/salon24_{datetime.today().date()}.xlsx", engine='xlsxwriter') as writer:    
     df.to_excel(writer, 'Posts', index=False)     
 
 
